878c6ac0a38e003976bd2ed68248ed0a77c7636b
ceejay <cvvjimenez@gmail.com>
2016-04-27 21:48:36 -0700

    ADDED pulling youtube data

--- src/main/java/edu/csula/datascience/acquisition/YouTubeSource.java
+++ src/main/java/edu/csula/datascience/acquisition/YouTubeSource.java
@@ -3,6 +3,8 @@ package edu.csula.datascience.acquisition;
         import com.google.api.client.googleapis.json.GoogleJsonResponseException;
         import com.google.api.client.http.HttpRequest;
         import com.google.api.client.http.HttpRequestInitializer;
+        import com.google.api.client.http.javanet.NetHttpTransport;
+        import com.google.api.client.json.jackson2.JacksonFactory;
         import com.google.api.services.youtube.YouTube;
         import com.google.api.services.youtube.model.*;
         import org.apache.http.HttpEntity;
@@ -21,29 +23,36 @@ package edu.csula.datascience.acquisition;
         import java.util.regex.Matcher;
         import java.util.regex.Pattern;
 
-public class YouTubeSource  implements Source<CommentThread> {
-
+public class YouTubeSource  implements Source<SearchResult> {
+    String query;
     YouTube youtube;
     Properties properties;
+    List<SearchResult> searchResultList;
 
     private static String FILE_PATH = "misc/youtube_key.properties";
+    private static long MAX_ITEMS = 2;
 
-//    public static void main(String[] args){
-//        YouTubeSource yts = new YouTubeSource();
-//    }
-
+    public static void main(String[] args){
+        String q = "dog";
+        YouTubeSource yts = new YouTubeSource(q);
+    }
 
     public YouTubeSource(String query){
+        this.query = query;
+
         // Masking developer key using a properties file
         properties = new Properties();
         try {
-            System.out.println(System.getProperty("user.dir"));
+            System.out.println("Properties file Path: " + System.getProperty("user.dir") + FILE_PATH);
             properties.load(new FileInputStream(FILE_PATH));
         } catch (IOException e) {
-            System.err.println("There was an error reading " + FILE_PATH + ": " + e.getCause()
-                    + " : " + e.getMessage());
-            System.exit(1);
+            e.printStackTrace();
         }
+
+        this.youtube = new YouTube.Builder(new NetHttpTransport(), new JacksonFactory(), request -> {
+        }).setApplicationName("youtube-creeper-data-science").build();
+
+        search();
 
     }
 
@@ -62,7 +71,50 @@ public class YouTubeSource  implements Source<CommentThread> {
      * @return TODO:: RETURN SOMEthing
      */
     @Override
-    public Collection<CommentThread> next(){
-        return null;
+    public Collection<SearchResult> next(){
+        List<SearchResult> searchResultList = null;
+        try {
+            YouTube.Search.List search = youtube.search().list("id,snippet");
+            search.setKey(this.properties.getProperty("api_key"));
+            search.setQ(this.query);
+            search.setMaxResults(MAX_ITEMS);
+            // Call the API and print results.
+            SearchListResponse response = search.execute();
+            searchResultList = response.getItems();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return searchResultList;
+    }
+
+    public void search(){
+        List<SearchResult> searchResultList = null;
+        try {
+            YouTube.Search.List search = youtube.search().list("id,snippet");
+            search.setKey(this.properties.getProperty("api_key"));
+            search.setQ(this.query);
+            search.setMaxResults(MAX_ITEMS);
+            // Call the API and print results.
+            SearchListResponse response = search.execute();
+            searchResultList = response.getItems();
+            Iterator<SearchResult> searchResultIterator = searchResultList.iterator();
+            while(searchResultIterator.hasNext()){
+                SearchResult video = searchResultIterator.next();
+                ResourceId rId = video.getId();
+                //determine if the video is a video
+                if (rId.getKind().equals("youtube#video")) {
+                    Thumbnail thumbnail = video.getSnippet().getThumbnails().getDefault();
+
+                    video.getSnippet().getDescription();
+                    System.out.println(" Video Id" + rId.getVideoId());
+                    System.out.println(" Title: " + video.getSnippet().getTitle());
+                    System.out.println(" Description: " + video.getSnippet().);
+                    System.out.println(" Thumbnail: " + thumbnail.getUrl());
+                    System.out.println("\n-------------------------------------------------------------\n");
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
