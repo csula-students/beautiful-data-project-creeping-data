e7f45ec65a1b207cafe4c9895c415fdc94a7f34f
cs454Andy <baghumyana@yahoo.com>
2016-04-30 03:06:23 -0700

    updated_by_andy

--- src/test/java/edu/csula/datascience/acquisition/MockCollector.java
+++ src/test/java/edu/csula/datascience/acquisition/MockCollector.java
@@ -1,23 +1,29 @@
 package edu.csula.datascience.acquisition;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.stream.Collectors;
 
 /**
  * A mock implementation of collector for testing
  */
-public class MockCollector implements Collector<SimpleModel, MockData> {
+public class MockCollector implements Collector<MockData, MockData> {
     @Override
-    public Collection<SimpleModel> mungee(Collection<MockData> src) {
-        // in your example, you might need to check src.hasNext() first
-        return src
-            .stream()
-            .filter(data -> data.getContent() != null)
-            .map(SimpleModel::build)
-            .collect(Collectors.toList());
-    }
+    public Collection<MockData> mungee(Collection<MockData> src) {
 
+        List<MockData> cleanedList = new ArrayList<>();
+
+        for (MockData video: src) {
+
+            if (video.title != null && video.likecount != null && video.viewCount != null) {
+                cleanedList.add(video);
+            }
+
+        }
+        return cleanedList;
+    }
     @Override
-    public void save(Collection<SimpleModel> data) {
+    public void save(Collection<MockData> data) {
     }
 }
